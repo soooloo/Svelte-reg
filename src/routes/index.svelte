ffa2ba9b970d1ec9744de4b909faa3a6387210a0
soolooo <Sulimvvn@icloud.com>
2022-05-03 15:09:56 +0200

    Project Upload

---
+++ src/routes/index.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Register from '../components/Register.svelte';
+	import Login from '../components/Signin.svelte';
+  import Welcome from '../components/Welcome.svelte';
+	let registered = false;
+  let loggedIn = false;
+  let loggedUser = '';
+</script>
+
+{#if registered && !loggedIn}
+	<Login bind:registered bind:loggedIn bind:loggedUser />
+{:else if !registered && !loggedIn}
+	<Register bind:registered />
+{:else if registered && loggedIn}
+  <Welcome loggedUser={loggedUser} />
+{/if}
